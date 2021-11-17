92afe2daa2016097fa9728335f2c6cd9589ca346
Jialugao <gaojialud@163.com>
2021-11-17 23:05:23 +0800

    add MT50, and support task name in directory name

--- single_train.sh
+++ single_train.sh
@@ -1,1 +1,2 @@
-python starter/single_task_sac.py --config meta_config/mt10/single_task_sac.json --id MT10_Single_Task --seed 42 --worker_nums 1 --eval_worker_nums 1 --no_cuda --task_name reach-v1
+source seed.sh
+python starter/single_task_sac.py --config meta_config/mt10/single_task_sac.json --id MT10_Single_Task --seed $SEED --worker_nums 1 --eval_worker_nums 1 --no_cuda --task_name reach-v1
